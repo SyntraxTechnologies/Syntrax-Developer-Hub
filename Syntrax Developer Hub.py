c742dc2c878a31d6acfc0f18f2c0c8f57d67606a
Syntrax <dannycloudstorage1@gmail.com>
2026-02-23 12:25:02 +0100

    Restore original code and add license details
    Restored original code and added licensing information.

--- Syntrax Developer Hub.py
+++ Syntrax Developer Hub.py
@@ -1,3 +1,21 @@
+
+#Copyright (C) 2026 Syntrax Technologies
+
+#This file is part of Syntrax Start Shell.
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU Affero General Public License as published by the
+#Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#See the GNU Affero General Public License for more details.
+
+#You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 import json, os, re, uuid, random, base64, difflib, subprocess, psutil, csv, requests
@@ -357,4 +375,5 @@ class SyntraxHub:
 if __name__ == "__main__":
     root = tk.Tk()
     app = SyntraxHub(root)
+
     root.mainloop()
